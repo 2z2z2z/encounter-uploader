75a93ab8e15ce3f27119912a0062fe0ba9abcf34
2z2z2z <seo2z@yandex.ru>
2025-07-17 14:05:13 +0300

    Add VersionLabel component to App.vue and declare Vue module in vite-env.d.ts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,10 +2,12 @@
   <div>
     <div class="container min-h-screen py-6">
       <router-view />
+      <VersionLabel />
     </div>
     <UploadProgress />
   </div>
 </template>
 <script setup lang="ts">
 import UploadProgress from './components/UploadProgress.vue'
+import VersionLabel from './components/VersionLabel.vue'
 </script>
